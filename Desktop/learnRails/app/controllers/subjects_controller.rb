96909ce879af2f2b11857101c570688d68509331
Nguyen Minh Duc <minhduc.xd.93@gmail.com>
2017-04-17 13:40:57 +0100

    learning about forms of Rails

--- Desktop/learnRails/app/controllers/subjects_controller.rb
+++ Desktop/learnRails/app/controllers/subjects_controller.rb
@@ -1,11 +1,15 @@
 class SubjectsController < ApplicationController
   def index
+    @subject = Subject.all
+    #render('index') #by default it will render index, so this is optional
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new(:name => "default")
   end
 
   def create
